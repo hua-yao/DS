6dd53ea0abdfcb466153992f0a152e0972dcf30c
huayao <3156490010@qq.com>
2020-04-15 13:46:34 +0800

    spu添加保存后台代码提交

--- gmall-bean/src/main/java/bean/SpuImage.java
+++ gmall-bean/src/main/java/bean/SpuImage.java
@@ -4,13 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 /**
  *spu图片信息保存
  * @author huayao
  */
 @Data
-public class SpuImage {
+public class SpuImage implements Serializable {
     @Id
     @Column
     private Long id;
