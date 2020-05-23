5f75ac9d5cd51fd0242201bed5e1324f6f89f721
huayao <3156490010@qq.com>
2020-05-23 22:58:34 +0800

    es

---
+++ gmall-list-service/src/test/java/com/huayao/gmalllistservice/GmallListServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.huayao.gmalllistservice;
+
+import io.searchbox.action.Action;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestResult;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallListServiceApplicationTests {
+
+
+    @Test
+    void contextLoads(){
+    }
+
+}
