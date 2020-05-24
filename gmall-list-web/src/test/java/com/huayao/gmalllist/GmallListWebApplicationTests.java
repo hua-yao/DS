b063fbbddf4c52561d9e74474db98b7067a3e255
huayao <3156490010@qq.com>
2020-05-24 17:18:59 +0800

    es保存数据

---
+++ gmall-list-web/src/test/java/com/huayao/gmalllist/GmallListWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huayao.gmalllist;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallListWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
